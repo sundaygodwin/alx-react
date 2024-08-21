eb7488f5f41304a388ba054d0759262f79b0ce5d
Geexix <gift85210@gmail.com>
2024-08-21 06:04:00 +0100

    update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,25 +1,17 @@
-import $ from "jquery";
+import $ from 'jquery';
 import _ from 'lodash';
 
-// Function to update the counter
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
 let count = 0;
+
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
-}
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
-// Debounced version of the updateCounter function
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
-
-// Document Ready Function
-$(document).ready(() => {
-    // Create and append elements
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button id="start-button">Click here to get started</button>');
-    $('body').append("<p id='count'></p>");
-    $('body').append('<p>Copyright - Holberton School</p>');
-  
-    // Bind the debounced function to the button click event
-    $('#start-button').on('click', debouncedUpdateCounter);
-  });
+$('button').on('click', _.debounce(updateCounter, 500));

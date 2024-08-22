af5a93e1c1e9afcfaf155d6e21205fc94efec8f6
Geexix <gift85210@gmail.com>
2024-08-22 15:28:40 +0100

    add css and images to webpack

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,30 +1,18 @@
-import $ from "jquery";
+import '../css/main.css';
+import $ from 'jquery';
 import _ from 'lodash';
 
-import '../css/main.css'; // Import your CSS file
-// import logo from '../assets/holberton-logo'; // Import an image file
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-// Function to update the counter
-let count = 0;
+let conunt = 0;
+
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
-}
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
-// Debounced version of the updateCounter function
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
-
-// Document Ready Function
-$(document).ready(() => {
-  // add logo to the start of document
-  $('body').prepend('<div id="logo"></div>');
-  // Create and append elements
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="start-button">Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  // Bind the debounced function to the button click event
-  $('#start-button').on('click', debouncedUpdateCounter);
-});
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,52 +1,38 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    assetModuleFilename: 'assets/[name][ext][query]' // Handle asset files
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
   },
-  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   module: {
     rules: [
-      // Rule for CSS files
       {
-        test: /\.css$/,
-        use: [
-          'style-loader', // Inject CSS into the DOM
-          'css-loader'    // Resolve CSS imports and URLs
-        ]
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-      // Rule for image files with optimization
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: 'asset/resource', // Emit the image file as a separate asset
+        test: /\.(gif|png|jp?g|svg)$/i,
         use: [
+          "file-loader",
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65 // Adjust quality for JPEG images
-              },
-              optipng: {
-                enabled: true, // Optimize PNG images
-              },
-              pngquant: {
-                quality: [0.65, 0.90], // Quality range for PNG images
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false, // Disable interlacing for GIFs
-              },
-              webp: {
-                quality: 75 // Quality for WebP images
-              }
-            }
-          }
-        ]
-      }
-    ]
-  }
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
